d7d2ec904f43e85dc1a0a006537de267f934ee41
bhavanisarma <bhavani.sarma@gmail.com>
2019-09-14 23:40:33 +0000

    first commit

---
+++ db/migrate/20190914225902_create_patients.rb
@@ -0,0 +1,12 @@
+class CreatePatients < ActiveRecord::Migration[5.2]
+  def change
+    create_table :patients do |t|
+    	t.string :name
+    	t.integer :healthcardnumber
+    	t.string :apptime
+
+
+      t.timestamps
+    end
+  end
+end
